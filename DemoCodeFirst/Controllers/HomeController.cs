53ce243ce704238df8a34c95f4afce39e542f699
Vishal Yewale <Vishal Yewale@IB-PUNE-LAP-491>
2024-05-09 19:31:57 +0530

    Add project files.

---
+++ DemoCodeFirst/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoCodeFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet("ShowName")]
+        public string FnShowName()
+        {
+            return "Hello World";
+        }
+
+    }
+}
